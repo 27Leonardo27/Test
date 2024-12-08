e2afdca3c6b1cca57c6242b5e42031ed7f36b60f
andrey dolgih <12dolg12@inbox.ru>
2024-12-08 23:22:35 +0300

    add Choice

--- TEST_proj2/Program.cs
+++ TEST_proj2/Program.cs
@@ -1,42 +1,65 @@
-﻿namespace TEST_proj2
+﻿using System.Diagnostics;
+
+namespace TEST_proj2
 {
     internal class Program  //Генератор пароля 
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your Login: ");
-            Console.ReadLine();
+            do
+            {
+                Console.Write("\n\nEnter '1' for continue or '2' for exit: ");
 
-            Console.Write("Enter your password lenght: ");
-            var length = int.Parse(Console.ReadLine());
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
 
-            string password = PasswordGenerator.GetPassword(length);
+                            Console.Write("Enter your Login: ");
+                            Console.ReadLine();
 
-            Console.Write("==============================\n Your password is:       ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write(password);
-            Console.ResetColor();
-            Console.WriteLine("\n==============================");
-        }
+                            Console.Write("Enter your password lenght: ");
+                            var length = int.Parse(Console.ReadLine());
 
-        class PasswordGenerator
-        {
-            static string chars = "abcdefgehjkuytropzxcvbnml:;'|<>{}%^&*()[]_!=+?1234567890";
+                            string password = PasswordGenerator.GetPassword(length);
 
-            public static string GetPassword(int length)
-            {                
-                var random = new Random();
-
-                string password = "";
-               
-                for (var i = 0; i < length; i++)
-                {                   
-                    var index = random.Next(chars.Length);
-                    password += chars[index].ToString();
+                            Console.Write("==============================\nYour password is:       ");
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write(password);
+                            Console.ResetColor();
+                            Console.WriteLine("\n==============================");
+                            break;
+                        }
+                    case "2":
+                        {
+                            return;
+                        }
                 }
-                
-                return password;
+
             }
+
+            while (true);
         }
+    }
+}
+
+
+class PasswordGenerator
+{
+    static string chars = "abcdefgehjkuytropzxcvbnmlABCDEFGEHJKUYTROPZXCVBNML%&!=*#?1234567890";
+
+    public static string GetPassword(int length)
+    {
+        var random = new Random();
+
+        string password = "";
+
+        for (var i = 0; i < length; i++)
+        {
+            var index = random.Next(chars.Length);
+            password += chars[index].ToString();
+        }
+
+        return password;
     }
 }
